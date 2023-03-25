7dd0a82a174545002b83999a601553d214df4932
alejo9010 <alejandrosanchez9010@gmail.com>
2023-03-25 10:36:45 -0500

    About page

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,53 @@
+import { motion } from 'framer-motion';
+
 type Props = {};
 
 function About({}: Props) {
-  return <div>About</div>;
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{ duration: 1.5 }}
+      className='h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
+      <motion.img
+        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56  rounded-full object-contain md:object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
+        src='/images/about_photo.jpg'
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        whileInView={{
+          x: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        viewport={{ once: true }}
+      />
+
+      <div className='space-y-10 px-0 md:px-10'>
+        <h4 className='text-4xl font-semibold'>
+          Here is a <span className='underline decoration-[#f7ab0a]/50'>little</span> background
+        </h4>
+        <p className='text-sm md:text-base'>
+          I am a web developer with a passion for coding. I have been coding for the past 8 years,
+          mainly working on hobby projects using C#. I always had an interest in technology and
+          programming, so it was only natural for me to pursue a career in this field. After moving
+          to the United States, I decided to make web development my specialty. I have become
+          proficient in a variety of programming languages, including HTML, CSS,
+          JavaScript/TypeScript, Tailwind and React/NextJS. I am always looking for new and exciting
+          ways to improve my skills and stay up-to-date with the latest trends and technologies in
+          the industry.
+        </p>
+      </div>
+    </motion.div>
+  );
 }
 
 export default About;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,15 +14,15 @@ function Hero({}: Props) {
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
       <BackgroundCircles />
       <Image
-        className='relative rounded-full h-32 w-32 mx-auto object-cover'
-        src='/images/alex_avatar.png'
+        className='relative rounded-full  h-32 w-32 mx-auto object-cover'
+        src='/images/alex_avatar.jpg'
         alt='Profile photo of alejandro sanchez'
         width={100}
         height={100}
       />
       <div className='z-20'>
         <h2 className='text-sm uppercase text-gray-500  pb-2 tracking-[15px]'>Software Engineer</h2>
-        <h1 className='text-5xl lg:text-6xl font-semibold scroll-px-10'>
+        <h1 className='text-2xl lg:text-6xl font-semibold scroll-px-10'>
           <span className='mr-3'>{text}</span>
           <Cursor cursorColor='#f7ab0a' />
         </h1>
