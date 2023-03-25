db17d239d70594432554d407a914b22ec75c5a34
alejo9010 <alejandrosanchez9010@gmail.com>
2023-03-25 15:19:33 -0500

    Project section

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -10,14 +10,16 @@ function Skill({ directionLeft }: Props) {
       <motion.img
         initial={{
           x: directionLeft ? -200 : 200,
+          opacity: 0,
         }}
         transition={{
-          duration: 1.5,
+          duration: 1,
         }}
         whileInView={{
           opacity: 1,
           x: 0,
         }}
+        viewport={{ once: true }}
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png'
         className='rounded-full border border-gray-500 object-cover w-24 h-24 md:h-28 md:w-28 xl:w-36 xl:h-36 filter group-hover:grayscale transition duration-300 ease-in-out'
         alt=''
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -15,6 +15,7 @@ function Skills({}: Props) {
       transition={{
         duration: 1.5,
       }}
+      viewport={{ once: true }}
       className='h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
       <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>Skills</h3>
       <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
